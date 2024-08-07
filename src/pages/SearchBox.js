4016f5514e25dc26f3b8d3343a5fdf8a31dd1102
maocao <uchuang9128@gmail.com>
2024-08-07 15:07:18 +0800

    init

---
+++ src/pages/SearchBox.js
@@ -0,0 +1,14 @@
+import CustomizedInputBase from "../components/InputField";
+
+
+
+function Search() {
+    return (
+      <div className="searchBox">
+        <CustomizedInputBase />
+      </div>
+    );
+  }
+  
+  export default Search;
+  
